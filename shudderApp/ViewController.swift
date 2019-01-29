941ec362efb1d4fa50bc4e1f9f86af2691300bd1
John King <johnhenryking@aol.com>
2019-01-29 10:43:05 -0500

    Initial Commit

---
+++ shudderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shudderApp
+//
+//  Created by User on 1/29/19.
+//  Copyright © 2019 jonathanking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
